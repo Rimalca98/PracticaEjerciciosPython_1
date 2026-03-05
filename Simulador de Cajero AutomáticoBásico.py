0d0c60db364550b96ffa1c5517e875b81d4a0f9f
Ricardo Maldonado <rimalca98@gmail.com>
2026-03-05 15:51:23 -0500

    Corregi el simulador de cajero automatico

--- Simulador de Cajero AutomáticoBásico.py
+++ Simulador de Cajero AutomáticoBásico.py
@@ -8,25 +8,46 @@ print("3. Depositar dinero")
 
 saldo = 1000
 
-opcion = input("Seleccione una opción: ")
+opcion = int(input("Seleccione una opción: "))
 
-if opcion == "1":
+while opcion < 1 or opcion > 3:
+    print ("Opción no válida.")
+    opcion = int(input("Seleccione una opción: "))
+
+if opcion == 1:
     print("Su saldo actual es de:  $", saldo)
-elif opcion == "2":
-    print("Usted ha seleccionado retirar dinero.")
-    retiro = int(input("Que cantidad va a retirar? $ "))
-    if retiro > 1000:
-        print("Fondos Insuficientes...")
-    
-    if retiro < 0:
-        print("Ingrese un monto valido...")
+    input("Gracias por usar el cajero automático, presiona enter para salir...")
+
+elif opcion == 2:
+        print("Usted ha seleccionado retirar dinero.")
+        retiro = int(input("Que cantidad va a retirar? $ "))
+
+        while retiro <= 0:
+            print("Monto invalido, Ingrese un monto valido...")
+            retiro = int(input("Que cantidad va a retirar? $ "))
+
+        while retiro > saldo:
+            print("Fondos Insuficientes...")
+            retiro = int(input("Que cantidad va a retirar? $ "))    
+            if retiro < 0:
+                print("Ingrese un monto valido...")
         
-    else:
-        print("Transaccion completada!")
-        print("Su nuevo saldo es de: $", saldo - retiro )
+        else:
+            print("Transaccion completada!")
+            print("Su nuevo saldo es de: $", saldo - retiro )
+            input("Gracias por usar el cajero automático, presiona enter para salir...")
 
 
-elif opcion == "3":
+elif opcion == 3:
     print("Usted ha seleccionado depositar dinero.")
-else:
-    print("Opción no válida.")
+    deposito = int(input("Que cantidad va a depositar? $ "))    
+    
+    while deposito <= 0:
+            print("Monto invalido, Ingrese un monto valido...")
+            deposito = int(input("Que cantidad va a depositar? $ "))
+    if deposito > 0:
+        print(f"Deposito correcto, nuevo saldo: $ {saldo + deposito}")
+        input("Gracias por usar el cajero automático, presiona enter para salir...")
+                
+
+
