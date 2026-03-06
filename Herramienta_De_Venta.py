7e5090915355f8ceddf8ea5e4293178da9076250
Ricardo Maldonado <rimalca98@gmail.com>
2026-03-06 15:58:40 -0500

    Se corrigen algunos errores de ingreso de datos

--- Herramienta_De_Venta.py
+++ Herramienta_De_Venta.py
@@ -5,8 +5,18 @@ print("="*35)
 
 nombre = input("Ingrese el nombre del cliente: ")
 
+
+while True:
+    if nombre.isalpha():
+        break
+    else:
+        print("Ingrese solo letras...")
+        nombre = input("Ingrese el nombre del cliente: ")
+
 precio_unidad = int(input("Precio por unidad: $"))
 cantidad = int(input("Cantidad de productos: "))
+
+
 
 soli_vip = input("El usuario tiene VIP, favor responder s/n en minuscula: ")
 vip = soli_vip == "s"
@@ -19,8 +29,8 @@ total = subtotal - descuento
 
 print("\nResumen de la Venta")
 print(f"\nNombre del cliente: {nombre}")
-print(f"Cantidad: {cantidad} unidad(es) x {precio_unidad:,}")
-print(f"Subtotal: {subtotal:,}")
+print(f"Cantidad: {cantidad} unidad(es) x $ {precio_unidad:,}")
+print(f"Subtotal: $ {subtotal:,}")
 
 if vip:
     print(f"Menos Descuento por VIP: {descuento:,} (10% aplicado)")
@@ -29,5 +39,5 @@ else:
 
 print(f"Total a pagar: $ {total:,}")
 
-#
+input("\nGracias por su compra. Presiona Enter para salir...")
 
--- Simulador de Cajero AutomáticoBásico.py
+++ Simulador de Cajero AutomáticoBásico.py
@@ -2,45 +2,51 @@ print("================================")
 print("Bienvenido al Cajero Automático")
 print("================================")
 
-print("1. Consultar saldo")
-print("2. Retirar dinero")
-print("3. Depositar dinero")
 
-saldo = 1000
+operacion = int(input("Cuantas transacciones deseas realizar?: "))
 
-opcion = int(input("Seleccione una opción: "))
+for i in range(operacion):
 
-while opcion < 1 or opcion > 3:
-    print ("Opción no válida.")
+
+    print("1. Consultar saldo")
+    print("2. Retirar dinero")
+    print("3. Depositar dinero")
+
+    saldo = 1000
+
     opcion = int(input("Seleccione una opción: "))
 
-if opcion == 1:
-    print(f"Su saldo actual es de:  ${saldo}")
+    while opcion < 1 or opcion > 3:
+        print ("Opción no válida.")
+        opcion = int(input("Seleccione una opción: "))
 
-elif opcion == 2:
-    print("Usted ha seleccionado retirar dinero.")
-    retiro = int(input("Que cantidad va a retirar? $ "))
+    if opcion == 1:
+        print(f"Su saldo actual es de:  $ {saldo:,}")
 
-    while retiro <= 0 or retiro > saldo:
-        if retiro <= 0:
-            print("Monto invalido, Ingrese un monto valido...")
-        else:
-            print(f"Saldo insuficiente, tu saldo es de: {saldo}")
-        retiro = int(input("Que cantidad desea retirar: $ "))
-    saldo -= retiro
-    print(f"Transaccion completada, su nuevo saldo es de: $ {saldo}")
+    elif opcion == 2:
+        print("Usted ha seleccionado retirar dinero.")
+        retiro = int(input("Que cantidad va a retirar? $ "))
+
+        while retiro <= 0 or retiro > saldo:
+            if retiro <= 0:
+                print("Monto invalido, Ingrese un monto valido...")
+            else:
+                print(f"Saldo insuficiente, tu saldo es de: {saldo}")
+            retiro = int(input("Que cantidad desea retirar: $ "))
+        saldo -= retiro
+        print(f"Transaccion completada, su nuevo saldo es de: $ {saldo}")
                  
 
 
-elif opcion == 3:
-    print("Usted ha seleccionado depositar dinero.")
-    deposito = int(input("Que cantidad va a depositar? $ "))    
+    elif opcion == 3:
+        print("Usted ha seleccionado depositar dinero.")
+        deposito = int(input("Que cantidad va a depositar? $ "))    
     
-    while deposito <= 0:
-        print("Monto invalido, Ingrese un monto valido...")
-        deposito = int(input("Que cantidad va a depositar? $ "))
-    saldo += deposito
-    print(f"Deposito correcto, nuevo saldo: $ {saldo}")
+        while deposito <= 0:
+            print("Monto invalido, Ingrese un monto valido...")
+            deposito = int(input("Que cantidad va a depositar? $ "))
+        saldo += deposito
+        print(f"Deposito correcto, nuevo saldo: $ {saldo}")
         
 input("\nGracias por usar el cajero automático. Presiona Enter para salir...")
                 
